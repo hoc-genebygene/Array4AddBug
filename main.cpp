759bcc54620a07037e2982c8136db7db4543d350
Ho Cheung <hoc@genebygene.com>
2018-05-03 15:04:58 -0500

    Final version

--- main.cpp
+++ main.cpp
@@ -21,22 +21,46 @@ static ArrayType static_b{4,0,0,0};
 static ArrayType static_c{4,0,0,0};
 static ArrayType static_d{4,0,0,0};
 
-std::array<uint8_t, 4> getOccCountOfRegion(uint32_t region) {
+void print_array(ArrayType arr) {
+    std::cout << (int)arr[0] << " " << (int)arr[1] << " " << (int)arr[2] << " " << (int)arr[3] << std::endl;
+}
+
+std::array<uint8_t, 4> add_some_stuff() {
     ArrayType a = static_a;
     ArrayType b = static_b;
-    const ArrayType & c = static_c;
-    const ArrayType & d = static_d;
+    ArrayType c = static_c;
+    ArrayType d = static_d;
+
+    // Also fixes bug...
+    // ArrayType a{3,1,0,0};
+    // ArrayType b{4,0,0,0};
+    // ArrayType c{4,0,0,0};
+    // ArrayType d{4,0,0,0};
+
+
+    print_array(a);
+    print_array(b);
+    print_array(c);
+    print_array(d);
 
     array4_add_inplace(a, c);
+    std::cout << std::endl;
+    print_array(a);
+    
     array4_add_inplace(b, d);
+    std::cout << std::endl;
+    print_array(b);
+
     array4_add_inplace(a, b);
+    print_array(a);
+
     return a;
 }
 
 int main() {
-    auto occ_count = getOccCountOfRegion(1);
+    auto count = add_some_stuff();
 
-    std::cout << (int)occ_count[0] << " " << (int)occ_count[1] << " " << (int)occ_count[2] << " " << (int)occ_count[3] << std::endl;
+    print_array(count);
 
     // Correct results: 15 1 0 0
     // Incorrect results: 8 1 0 0
