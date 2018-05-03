53aebb4326bd62a8898d684ad574b49b0e09cef2
Ho Cheung <hoc@genebygene.com>
2018-05-03 14:54:42 -0500

    Update

--- main.cpp
+++ main.cpp
@@ -21,52 +21,20 @@ static ArrayType static_b{4,0,0,0};
 static ArrayType static_c{4,0,0,0};
 static ArrayType static_d{4,0,0,0};
 
-struct Reference {
-    static std::array<std::array<uint8_t, 4>,256> countLookup;
+std::array<uint8_t, 4> getOccCountOfRegion(uint32_t region) {
+    ArrayType a = static_a;
+    ArrayType b = static_b;
+    const ArrayType & c = static_c;
+    const ArrayType & d = static_d;
 
-    static std::array<uint8_t, 4> getOccCountOfRegion(uint32_t region) {
-//        std::array<uint8_t, 4> a = countLookup[region & 255];
-//        std::array<uint8_t, 4> b = countLookup[(region >> 8) & 255];
-//        const std::array<uint8_t, 4> & c = countLookup[(region >> 2*8) & 255];
-//        const std::array<uint8_t, 4> & d = countLookup[region >> 3*8];
-
-        ArrayType a = static_a;
-        ArrayType b = static_b;
-        const ArrayType & c = static_c;
-        const ArrayType & d = static_d;
-
-        array4_add_inplace(a, c);
-        array4_add_inplace(b, d);
-        array4_add_inplace(a, b);
-        return a;
-    }
-};
-
-std::array<std::array<uint8_t, 4>,256> makeCountLookupArray() {
-    std::array<std::array<uint8_t, 4>,256> countLookup;
-
-    // Initialize the count lookup
-    const constexpr int numberOfBitsInIndex = 8;
-    for (int i = 0; i < 256; ++i) {
-        std::array<uint8_t, 4> allBaseCount = {{0}};
-        for (int base = 0; base < 4; ++base) {
-            uint32_t baseCount = 0;
-            for (int j = 0; j < numberOfBitsInIndex; j += 2) {
-                baseCount += (((i >> j) & 15) == base);
-            }
-            // Shift the count into its proper position
-            allBaseCount[base] |= baseCount;
-        }
-        countLookup[i] = allBaseCount;
-    }
-
-    return countLookup;
+    array4_add_inplace(a, c);
+    array4_add_inplace(b, d);
+    array4_add_inplace(a, b);
+    return a;
 }
 
-std::array<std::array<uint8_t, 4>,256> Reference::countLookup = makeCountLookupArray();
-
 int main() {
-    auto occ_count = Reference::getOccCountOfRegion(1);
+    auto occ_count = getOccCountOfRegion(1);
 
     std::cout << (int)occ_count[0] << " " << (int)occ_count[1] << " " << (int)occ_count[2] << " " << (int)occ_count[3] << std::endl;
 
